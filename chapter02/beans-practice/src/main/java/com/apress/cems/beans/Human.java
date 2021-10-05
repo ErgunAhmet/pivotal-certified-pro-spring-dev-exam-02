2893b5acc83a6b7930b2ef7e4803d226601f6693
ErgunAhmet <AhmetErgun2000@protonmail.com>
2021-10-05 17:16:46 +0200

    exercise p75 bean definitions

--- chapter02/beans-practice/src/main/java/com/apress/cems/beans/Human.java
+++ chapter02/beans-practice/src/main/java/com/apress/cems/beans/Human.java
@@ -1,9 +1,13 @@
 package com.apress.cems.beans;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Iuliana Cosmina
  * @since 1.0
  */
+@Component
 public interface Human {
 
     Item getItem();
